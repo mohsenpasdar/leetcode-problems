0fe1acd7fa0af9bf2f10cb8d641fb03ea471de0d
mohsen <mohsenput2@gmail.com>
2025-04-06 14:15:52 -0400

    problem 60 -permutation sequence - non optimum solution

---
+++ 60.py
@@ -0,0 +1,46 @@
+class Solution(object):
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        # res = []
+        # nums = [i for i in range(1, n + 1)]
+                
+        # def helper(permutation, remainingNums):
+        #     if len(remainingNums) == 0:
+        #         res.append(permutation[:])
+        #         return
+                
+        #     for i in range(len(remainingNums)):
+                
+        #         newPermutation = permutation + [remainingNums[i]]
+        #         newRemainingNums = remainingNums[:i] + remainingNums[i + 1:]
+        #         helper(newPermutation, newRemainingNums)
+        
+        # helper([], nums)
+        # return ''.join(str(num) for num in res[k - 1])
+        
+        nums = [i for i in range(1, n + 1)]
+    
+                
+        def helper(permutation, remainingNums, k, current):
+            if len(remainingNums) == 0:
+                return (current + 1, permutation)
+                
+            for i in range(len(remainingNums)):
+                
+                newPermutation = permutation + [remainingNums[i]]
+                newRemainingNums = remainingNums[:i] + remainingNums[i + 1:]
+                (current, newPermutation) = helper(newPermutation, newRemainingNums, k, current)
+                if current == k:
+                    return (current, newPermutation)
+            return (current, newPermutation)
+        
+        (current, newPermutation) = helper([], nums, k, 0)
+        return ''.join(str(num) for num in newPermutation)           
+
+
+solution = Solution()
+print(solution.getPermutation(9, 960))
