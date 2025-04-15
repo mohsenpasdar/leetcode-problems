f38a6b3c3a1ab3476d17a524f1ce168b8117cb1f
mohsen <mohsenput2@gmail.com>
2025-04-14 20:56:22 -0400

    python solutions for problem 79 leetcode

---
+++ 79.py
@@ -0,0 +1,64 @@
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        existes = False
+        visited_set = set()
+        
+        rows = len(board)
+        cols = len(board[0])
+        
+        def search(i, j, string):
+            temp = False
+            visited_set.add((i, j))
+            
+            if len(string) == 0:
+                return True
+            
+            if i > 0 and board[i - 1][j] == string[0] and (i - 1, j) not in visited_set:
+                temp = search(i - 1, j, string[1:])
+                if temp == True:
+                    return True
+            
+            if i < rows - 1 and board[i + 1][j] == string[0] and (i + 1, j) not in visited_set:
+                temp = search(i + 1, j, string[1:])
+                if temp == True:
+                    return True
+            
+            if j > 0 and board[i][j - 1] == string[0] and (i, j - 1) not in visited_set:
+                temp = search(i, j - 1, string[1:])
+                if temp == True:
+                    return True
+            
+            if j < cols - 1 and board[i][j + 1] == string[0] and (i, j + 1) not in visited_set:
+                temp = search(i, j + 1, string[1:])
+                if temp == True:
+                    return True
+            
+            visited_set.remove((i, j))
+            return False
+            
+        
+        
+        for row in range(rows):
+            for col in range(cols):
+                if word[0] == board[row][col]:
+                    exists = search(row, col, word[1:])
+                    if exists == True:
+                        return True
+                    
+        return existes
+        
+    
+        
+            
+        
+        
+solution = Solution()
+print(solution.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], "ABCB"))
+
+# a = [1, 2, 3]
+# print(a[3:])
