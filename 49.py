29c7490515253f5af9996af1b795531399923422
mohsen <mohsenput2@gmail.com>
2025-05-28 20:37:35 -0400

    solution to leetcode problem: 49 - dic and list to tuples

--- 49.py
+++ 49.py
@@ -11,6 +11,25 @@ class Solution:
 
         return list(anagram_groups.values())
     
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_groups = {}
+        
+        for current_str in strs:
+            current_str_list = [0] * 26
+            for char in current_str:
+                current_str_list[ord(char) - 97] += 1
+            
+            current_str_tuple = tuple(current_str_list)
+
+            if current_str_tuple in anagram_groups:
+                anagram_groups[current_str_tuple].append(current_str)
+            else:
+                anagram_groups[current_str_tuple] = [current_str]
+
+        return list(anagram_groups.values())
 solution = Solution()
-print(solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+print(solution.groupAnagrams(["bdddddddddd","bbbbbbbbbbc"]))
 # print(''.join(sorted("mohsen")))
+
