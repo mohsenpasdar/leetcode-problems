ad5f06f7a4c65f3d739015d5d9ce11ce4ea9a184
mohsen <mohsenput2@gmail.com>
2025-05-29 19:35:43 -0400

    solution to leetcode problem: 238 using prefix-postfix

--- 238.py
+++ 238.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-class Solution:
+class Solution2:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
         output = [0] * n
@@ -27,5 +27,23 @@ class Solution:
         
         return output
     
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        output = [0] * n
+        
+        output[0] = nums[0]
+        for i in range(1, n):
+            output[i] = nums[i] * output[i - 1]
+        
+        mult = 1
+        for j in range(n - 1, 0, -1):
+            output[j] = output[j - 1] * mult
+            mult *= nums[j]
+        
+        output[0]  = mult
+    
+        return output
+            
 solution = Solution()
 print(solution.productExceptSelf([-1,1,5,-3,2]))
