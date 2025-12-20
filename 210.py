4215c6edec59d439b3c99938c267e507f79b6bae
mohsen <mohsenput2@gmail.com>
2025-12-20 10:27:15 -0500

    problem 210 - topological sort - Kahn algo

--- 210.py
+++ 210.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import List
 
 class Solution:
@@ -36,4 +37,40 @@ class Solution:
         
         
 solution = Solution()
-print(solution.findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
+# print(solution.findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
+
+
+class Solution2:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
+            indegree[course] += 1
+            
+             
+        queue = deque()
+        output = []
+            
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+                
+        while queue:
+            course = queue.pop()
+            output.append(course)
+            
+            for neighbor in graph[course]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.appendleft(neighbor)
+        
+        if len(output) == numCourses:
+            return output
+        else:
+            return []
+
+        
+solution2 = Solution2()
+print(solution2.findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
